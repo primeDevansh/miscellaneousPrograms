cc46097468c371d288a093d23267687f873988de
primeDevansh <dvcam.rastogi1980@gmail.com>
2024-01-20 13:13:19 +0530

    Till 9 Digit Number

--- NoToWo.c
+++ NoToWo.c
@@ -3,31 +3,31 @@
 
 void spellOnesDigit(char ch) {
     switch(ch) {
-        case 1:
+        case '1':
             printf("one ");
             break;
-        case 2:
+        case '2':
             printf("two ");
             break;
-        case 3:
+        case '3':
             printf("three ");
             break;
-        case 4:
+        case '4':
             printf("four ");
             break;
-        case 5:
+        case '5':
             printf("five ");
             break;
-        case 6:
+        case '6':
             printf("six ");
             break;
-        case 7:
+        case '7':
             printf("seven ");
             break;
-        case 8:
+        case '8':
             printf("eight ");
             break;
-        case 9:
+        case '9':
             printf("nine ");
             break;
     }
@@ -36,31 +36,31 @@ void spellOnesDigit(char ch) {
 
 void spellTensDigit(char ch) {
     switch(ch) {
-        case 1:
+        case '1':
             printf("ten ");
             break;
-        case 2:
+        case '2':
             printf("twenty ");
             break;
-        case 3:
+        case '3':
             printf("thirty ");
             break;
-        case 4:
+        case '4':
             printf("forty ");
             break;
-        case 5:
+        case '5':
             printf("fifty ");
             break;
-        case 6:
+        case '6':
             printf("sixty ");
             break;
-        case 7:
+        case '7':
             printf("seventy ");
             break;
-        case 8:
+        case '8':
             printf("eighty ");
             break;
-        case 9:
+        case '9':
             printf("ninety ");
             break;
     }
@@ -69,31 +69,31 @@ void spellTensDigit(char ch) {
 
 void spellEleven(char ch) {
     switch(ch) {
-        case 1:
+        case '1':
             printf("eleven ");
             break;
-        case 2:
+        case '2':
             printf("twelve ");
             break;
-        case 3:
+        case '3':
             printf("thirteen ");
             break;
-        case 4:
+        case '4':
             printf("fourteen ");
             break;
-        case 5:
+        case '5':
             printf("fifteen ");
             break;
-        case 6:
+        case '6':
             printf("sixteen ");
             break;
-        case 7:
+        case '7':
             printf("seventeen ");
             break;
-        case 8:
+        case '8':
             printf("eighteen ");
             break;
-        case 9:
+        case '9':
             printf("nineteen ");
             break;
     }
@@ -101,6 +101,10 @@ void spellEleven(char ch) {
 }
 
 void NoToWo(int no) {
+    if(no > 999999999) {
+        printf("Number out of Range! Cannot Convert!\n");
+        return;
+    }
     if(no == 0) {
         printf("zero ");
         return;
@@ -109,10 +113,13 @@ void NoToWo(int no) {
     int i = 0, j = 0, k = 0;
     int strSize = 0;
 
-    if(no < 0)
-        str[i++] = '-';
+    if(no < 0) {
+        no *= -1;
+        printf("minus ");
+    }
+    // str[i++] = '-';
     while(no) {
-        str[i++] = no % 10;
+        str[i++] = (no % 10) + 48;
         no /= 10;
     }
     str[i] = '\0';
@@ -132,7 +139,8 @@ void NoToWo(int no) {
                 spellOnesDigit(str[i]);
                 break;
             case 2:
-                if(str[i] == 1 && str[i + 1] > 0) {
+            //48 is the ASCII value of 0; str > 48 => str > '0'
+                if(str[i] == '1' && str[i + 1] > 48) {
                     spellEleven(str[++i]);
                     continue;
                 }
@@ -147,7 +155,7 @@ void NoToWo(int no) {
                 printf("thousand ");
                 break;
             case 5:
-                if(str[i] == 1 && str[i + 1] > 0) {
+                if(str[i] == '1' && str[i + 1] > 48) {
                     spellEleven(str[++i]);
                     printf("thousand ");
                     continue;
@@ -161,7 +169,7 @@ void NoToWo(int no) {
                 printf("lakh ");
                 break;
             case 7:
-                if(str[i] == 1 && str[i + 1] > 0) {
+                if(str[i] == '1' && str[i + 1] > 48) {
                     spellEleven(str[++i]);
                     printf("lakh ");
                     continue;
@@ -169,6 +177,20 @@ void NoToWo(int no) {
                 spellTensDigit(str[i]);
                 spellOnesDigit(str[++i]);
                 printf("lakh ");
+                break;
+            case 8:
+                spellOnesDigit(str[i]);
+                printf("crore ");
+                break;
+            case 9:
+                if(str[i] == '1' && str[i + 1] > 48) {
+                    spellEleven(str[++i]);
+                    printf("crore ");
+                    continue;
+                }
+                spellTensDigit(str[i]);
+                spellOnesDigit(str[++i]);
+                printf("crore ");
                 break;
             default: 
                 continue;
